a42306d597b854d6d97266273a1002c2c8f5faa4
amirholya01 <amirholya01@gmail.com>
2024-11-22 11:15:13 +0100

    Interfaces in Persistence layer

---
+++ Src/PetShop.Core/PetShop.Application/Interfaces/Contexts/IDataBaseContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PetShop.Domain.Entities.Users;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace PetShop.Application.Interfaces.Contexts
+{
+    public interface IDataBaseContext
+    {
+         DbSet<User> Users { get; set; }
+         DbSet<Role> Roles { get; set; }
+         DbSet<UserInRole> UserInRoles { get; set; }
+        int SaveChanges(bool AcceptAllChangesOnSuccess);
+        int SaveChanges();
+        Task<int> SaveChangesAsync(bool AcceptAllChangesOnSuccess, CancellationToken cancellationToken = new CancellationToken());
+        Task<int> SaveChangesAsync(CancellationToken cancellationToken = new CancellationToken());
+    }
+}
