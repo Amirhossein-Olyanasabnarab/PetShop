e44d615e12337dce810f26c9aa3233b038a38343
amirholya01 <amirholya01@gmail.com>
2024-11-22 10:28:19 +0100

    Admin template

--- Src/PetShop.Presentation/EndPoint.Site/Startup.cs
+++ Src/PetShop.Presentation/EndPoint.Site/Startup.cs
@@ -47,6 +47,11 @@ namespace EndPoint.Site
                 endpoints.MapControllerRoute(
                     name: "default",
                     pattern: "{controller=Home}/{action=Index}/{id?}");
+
+                endpoints.MapControllerRoute(
+                name: "areas",
+                pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}"
+          );
             });
         }
     }
